40f1f5f21f7afc83f95096b48bf7cbb4a0d356ae
vesrntechnologies <vennjoy@gmail.com>
2025-06-13 15:17:32 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,602 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Which structure of steel contain 0% carbon?",
+    immg: "img1",
+    answer: "Ferrite",
+    options: [
+      "Ferrite",
+      "Pearlite",
+      "Austenite",
+      "Cementite",
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is the name of the key shown in the figure?",
+    immg: "img2",
+    answer: "Parallel sunk key",
+    options: [
+      "Parallel sunk key",
+      "Gib head key",
+      "Wood ruff key",
+      "Tapper sunk key",
+    ]
+  },
+  {
+    numb: 3,
+    question: "What is the purpose of annealing?",
+    immg: "img3",
+    answer: "To soften the steel",
+    options: [
+      "To soften the steel",
+      "To add cutting ability",
+      "To increase wear resistance",
+      "To refine the grain structure of the steel",
+    ]
+  },
+  {
+    numb: 4,
+    question: "What is the name of the structure if hot steel is rapidly cooled?",
+    immg: "img4",
+    answer: "Martensite",
+    options: [
+      "Ferrite",
+      "Austenite",
+      "Martensite",
+      "Pearlite",
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which process is adapted to add cutting ability and wear resistance on steel?",
+    immg: "img5",
+    answer: "Hardening",
+    options: [
+      "Tempering",
+      "Annealing",
+      "Normalising",
+      "Hardening",
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which heat treatment process affects the strength, toughness and ductility of steel?",
+    immg: "img6",
+    answer: "Hardening",
+    options: [
+      "Tempering",
+      "Hardening",
+      "Annealing",
+      "Normalising",
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is used to achieve faster rate of cooling in the quenching process?",
+    immg: "img7",
+    answer: "Brine solution",
+    options: [
+      "Oil",
+      "Air",
+      "Water",
+      "Brine solution",
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the tempering colour of taps and shear blades?",
+    immg: "img8",
+    answer: "Brown",
+    options: [
+      "Pale straw",
+      "Dark blue",
+      "Reddish",
+      "Brown",
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the soaking time allowed for 10 mm thickness steel during hardening?",
+    immg: "img9",
+    answer: "5 minutes",
+    options: [
+      "7 minutes",
+      "6 minutes",
+      "5 minutes",
+      "8 minutes",
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the cooling rate of carbon steel during the annealing?",
+    immg: "img10",
+    answer: "100°C to 150°C / hr",
+    options: [
+      "75°C to 100°C / hr",
+      "80°C to 100°C / hr",
+      "125°C to 150°C / hr",
+      "100°C to 150°C / hr",
+    ]
+  },
+  {
+    numb: 11,
+    question: "Which material is soft and ductile but relatively weak?",
+    immg: "img11",
+    answer: "Ferrite",
+    options: [
+      "Ferrite",
+      "Cementite",
+      "Pearlite",
+      "Hypereutectoid",
+    ]
+  },
+  {
+    numb: 12,
+    question: "What is known as 0.84% carbon steel?",
+    immg: "img12",
+    answer: "Pearlite steel",
+    options: [
+      "Cementite",
+      "Ferrite",
+      "Pearlite steel",
+      "Hypereutectoid",
+    ]
+  },
+  {
+    numb: 13,
+    question: "What is the name of the limit gauge shown in the figure?",
+    immg: "img13",
+    answer: "Progressive plug gauge",
+    options: [
+      "Plain ring gauge",
+      "Taper plug gauge",
+      "Progressive plug gauge",
+      "Double ended plug gauge",
+    ]
+  },
+  {
+    numb: 14,
+    question: "What is the purpose of ring gauge?",
+    immg: "img14",
+    answer: "To check the shaft diameter",
+    options: [
+      "To check the hole diameter",
+      "To check the shaft diameter",
+      "To check the tapered shaft diameter",
+      "To check the internal thread",
+    ]
+  },
+  {
+    numb: 15,
+    question: "What is the name of the gauge shown in the figure?",
+    immg: "img15",
+    answer: "Taper ring gauge",
+    options: [
+      "Ring gauge",
+      "Snap gauge",
+      "Taper ring gauge",
+      "Thread plug gauge",
+    ]
+  },
+  {
+    numb: 16,
+    question: "What is the name of the gauge shown in the figure?",
+    immg: "img16",
+    answer: "Thread plug gauge",
+    options: [
+      "Thread ring gauge",
+      "Thread plug gauge",
+      "Screw pitch gauge",
+      "Thread caliper gauge",
+    ]
+  },
+  {
+    numb: 17,
+    question: "What is the purpose of double row ball bearing?",
+    immg: "img17",
+    answer: "To carry radial and thrust load",
+    options: [
+      "To carry axial load",
+      "To carry radial load",
+      "To carry bi-directional axial load",
+      "To carry radial and thrust load",
+    ]
+  },
+  {
+    numb: 18,
+    question: "What is the name of the bearing shown in the figure?",
+    immg: "img18",
+    answer: "Needle bearing",
+    options: [
+      "Needle bearing",
+      "Thrust ball bearing",
+      "Taper roller bearing",
+      "Angular contact ball bearing",
+    ]
+  },
+  {
+    numb: 19,
+    question: "What is the name of the part marked as X shown in the figure?",
+    immg: "img19",
+    answer: "Ball separating gauge",
+    options: [
+      "Ball",
+      "Inner race",
+      "Outer race",
+      "Ball separating gauge",
+    ]
+  },
+  {
+    numb: 20,
+    question: "Which bearing material is used for light load and low speed application?",
+    immg: "img20",
+    answer: "Cast iron",
+    options: [
+      "Cast iron",
+      "Tin bronze",
+      "Cadmium based alloy",
+      "Copper and lead alloys",
+    ]
+  },
+  {
+    numb: 21,
+    question: "Which bearing carries the load parallel to its axis?",
+    immg: "img21",
+    answer: "Thrust bearing",
+    options: [
+      "Split bearing",
+      "Plain bearing",
+      "Thrust bearing",
+      "Radial bearing",
+    ]
+  },
+  {
+    numb: 22,
+    question: "Which type of bearing is used for taking high axial thrust load?",
+    immg: "img22",
+    answer: "Tapered roller bearing",
+    options: [
+      "Roller bearing",
+      "Tapered roller bearing",
+      "Self align roller bearing",
+      "Angular contact ball bearing",
+    ]
+  },
+  {
+    numb: 23,
+    question: "What is the name of the bearing shown in the figure?",
+    immg: "img23",
+    answer: "Thrust bearing",
+    options: [
+      "Ball bearing",
+      "Roller bearing",
+      "Thrust bearing",
+      "Journal bearing",
+    ]
+  },
+  {
+    numb: 24,
+    question: "What is the name of the bearing shown in the figure?",
+    immg: "img24",
+    answer: "Journal bearing",
+    options: [
+      "Split bearing",
+      "Bush bearing",
+      "Solid bearing",
+      "Journal bearing",
+    ]
+  },
+  {
+    numb: 25,
+    question: "Which bearing is used in the limited bearing space?",
+    immg: "img25",
+    answer: "Needle bearing",
+    options: [
+      "Needle bearing",
+      "Thrust ball bearing",
+      "Taper roller bearing",
+      "Angular contact ball bearing",
+    ]
+  },
+  {
+    numb: 26,
+    question: "Why extra clearance is provided between bearing and journal in the aluminium alloy bearing?",
+    immg: "img26",
+    answer: "To overcome high thermal expansion",
+    options: [
+      "Lubricant to flow freely",
+      "For corrosion resistance",
+      "To prevent wear resistance",
+      "To overcome high thermal expansion",
+    ]
+  },
+  {
+    numb: 27,
+    question: "What is the name of the gauge shown in the figure?",
+    immg: "img27",
+    answer: "Adjustable snap gauge",
+    options: [
+      "Plain snap gauge",
+      "Adjustable snap gauge",
+      "Threaded snap gauge",
+      "Caliper gauge",
+    ]
+  },
+  {
+    numb: 28,
+    question: "What is the name of the gauge shown in the figure?",
+    immg: "img28",
+    answer: "Threaded snap gauge",
+    options: [
+      "Plain snap gauge",
+      "Adjustable snap gauge",
+      "Threaded snap gauge",
+      "Caliper gauge",
+    ]
+  },
+  {
+    numb: 29,
+    question: "What is the name of the part marked as X shown in the figure?",
+    immg: "img29",
+    answer: "Workpiece",
+    options: [
+      "Ring gauge",
+      "Plug gauge",
+      "Thread gauge",
+      "Workpiece",
+    ]
+  },
+  {
+    numb: 30,
+    question: "What is the name of the part marked as X shown in the figure?",
+    immg: "img30",
+    answer: "Ball cage",
+    options: [
+      "Inner race",
+      "Outer race",
+      "Ball cage",
+      "Ball",
+    ]
+  },
+  {
+    numb: 31,
+    question: "What is the name of the bearing shown in the figure?",
+    immg: "img31",
+    answer: "Special thrust bearing",
+    options: [
+      "Roller bearing",
+      "Double roller bearing",
+      "Thrust bearing",
+      "Special thrust bearing",
+    ]
+  },
+  {
+    numb: 32,
+    question: "What is the name of the bearing shown in the figure?",
+    immg: "img32",
+    answer: "Self aligning roller contact bearing",
+    options: [
+      "Roller bearing",
+      "Needle bearing",
+      "Angular contact bearing",
+      "Self aligning roller contact bearing",
+    ]
+  },
+  {
+    numb: 33,
+    question: "What is the name of the bearing shown in the figure?",
+    immg: "img33",
+    answer: "Self aligning ball bearing",
+    options: [
+      "Solid bearing",
+      "Double row ball bearing",
+      "Single row ball bearing",
+      "Self aligning ball bearing",
+    ]
+  },
+  {
+    numb: 34,
+    question: "What is the name of the bearing shown in the figure?",
+    immg: "img34",
+    answer: "Self aligning bush bearing",
+    options: [
+      "Plain bearing",
+      "Bush bearing",
+      "Solid bearing",
+      "Self aligning bush bearing",
+    ]
+  },
+  {
+    numb: 35,
+    question: "What is the name of the part marked as X shown in the figure?",
+    immg: "img35",
+    answer: "Puller plate",
+    options: [
+      "Leg",
+      "Puller",
+      "Bearing",
+      "Puller plate",
+    ]
+  },
+  {
+    numb: 36,
+    question: "Which ball bearings are used to accommodate slight misalignments of the shaft?",
+    immg: "img36",
+    answer: "Self aligning ball bearing",
+    options: [
+      "Plain bearing",
+      "Ball bearing",
+      "Double row ball bearing",
+      "Self aligning ball bearing",
+    ]
+  },
+  {
+    numb: 37,
+    question: "Which bearing material has poor thermal conductivity?",
+    immg: "img37",
+    answer: "Laminated phenolic",
+    options: [
+      "Nylon",
+      "Teflon",
+      "Sintered alloys",
+      "Laminated phenolic",
+    ]
+  },
+  {
+    numb: 38,
+    question: "Which alloying metal bearing can work at higher temperature and higher load carrying capacity?",
+    immg: "img38",
+    answer: "Copper lead alloys",
+    options: [
+      "Cast iron",
+      "White metal",
+      "Copper lead alloys",
+      "Cadmium based alloy",
+    ]
+  },
+  {
+    numb: 39,
+    question: "What are the various stages of heat treatment?",
+    immg: "img39",
+    answer: "Heating, Soaking and Quenching",
+    options: [
+      "Heating, Cooling and Quenching",
+      "Quenching, Cooling and Heating",
+      "Heating, Soaking and Quenching",
+      "Soaking, Quenching and Cooling",
+    ]
+  },
+  {
+    numb: 40,
+    question: "What is the name of the structure formed, if a steel is heated for about 723°C?",
+    immg: "img40",
+    answer: "Austenite",
+    options: [
+      "Cementide",
+      "Austenite",
+      "Martensite",
+      "Ferrite",
+    ]
+  },
+  {
+    numb: 41,
+    question: "What is colour of a metal piece when heated to 250°C while doing the tempering process?",
+    immg: "img41",
+    answer: "Brown",
+    options: [
+      "Blue",
+      "Brown",
+      "Purple",
+      "Pale",
+    ]
+  },
+  {
+    numb: 42,
+    question: "Which authority publishes schedule of rates?",
+    immg: "img42",
+    answer: "Government department",
+    options: [
+      "Individual",
+      "Corporate",
+      "Partnership firm",
+      "Government department",
+    ]
+  },
+  {
+    numb: 43,
+    question: "What is the name of a booklet, the rates of various terms are indicated?",
+    immg: "img43",
+    answer: "Price catalogue",
+    options: [
+      "Price bank",
+      "Price bunch",
+      "Price tag",
+      "Price catalogue",
+    ]
+  },
+  {
+    numb: 44,
+    question: "What is the other term of pocket reference in engineering works?",
+    immg: "img44",
+    answer: "Hand book",
+    options: [
+      "Hand tool",
+      "Hand book",
+      "Good book",
+      "New book",
+    ]
+  },
+  {
+    numb: 45,
+    question: "Pipe cutting die is generally_______",
+    immg: "img45",
+    answer: "divided in two parts",
+    options: [
+      "divided in four parts",
+      "undivided",
+      "divided in three parts",
+      "divided in two parts",
+    ]
+  },
+  {
+    numb: 46,
+    question: "Which symbol is used for butt joints?",
+    immg: "img46",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      "",
+    ]
+  },
+  {
+    numb: 47,
+    question: "Which symbol is used for lap joint?",
+    immg: "img47",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      "",
+    ]
+  },
+  {
+    numb: 48,
+    question: "This type of section is limited by a break line____",
+    immg: "img48",
+    answer: "broken-out section",
+    options: [
+      "removed section",
+      "revolved section",
+      "broken-out section",
+      "half section",
+    ]
+  },
+  {
+    numb: 49,
+    question: "Objects that are symmetric can be shown effectively using this type of section_____",
+    immg: "img49",
+    answer: "half section",
+    options: [
+      "quarter section",
+      "full section",
+      "half section",
+      "symmetric section",
+    ]
+  },
+  {
+    numb: 50,
+    question: "In this type of section, one quarter of the object is removed…",
+    immg: "img50",
+    answer: "quarter section",
+    options: [
+      "revolved section",
+      "removed section",
+      "quarter section",
+      "half section",
+    ]
+  },
+];
